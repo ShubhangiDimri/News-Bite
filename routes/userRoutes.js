330a7b2571b6275d3f5f90ee0e73f05ed0ed27ea
ShubhangiDimri <shubhangidimri26@gmail.com>
2025-11-08 02:03:09 +0530

    added comment replies and voting feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,11 +5,21 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
+// Existing routes
 router.post("/comment", authMiddleware, comment);
 router.post("/like", authMiddleware, like);
 router.post("/activity", authMiddleware, activity);
 router.delete("/deleteComment", authMiddleware, deleteComment);
 router.put("/editProfile", authMiddleware, editProfile);
+
+// New comment reply routes
+router.post("/comments/:commentId/reply", authMiddleware, require("../controllers/userController").addReply);
+router.delete("/comments/:commentId/replies/:replyId", authMiddleware, require("../controllers/userController").deleteReply);
+router.get("/comments/:commentId/replies", authMiddleware, require("../controllers/userController").getReplies);
+
+// New voting routes
+router.post("/comments/:commentId/vote", authMiddleware, require("../controllers/userController").voteComment);
+router.post("/comments/:commentId/replies/:replyId/vote", authMiddleware, require("../controllers/userController").voteReply);
 
 router.get("/viewProfile", viewProfile);
 router.get("/search", searchNews);
